85b69a6ae98f32ec1371c135c23740ae844088b0
Antonio <j.ant.r.mer@gmail.com>
2017-06-30 15:15:10 -0500

    Se agrega pagina de Hola Mundo en HTML Basico y se crea una pagina index para ir agregando ejemplos del Taller

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>WEB Basico</title>
+
+	<link rel="stylesheet" href="css/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/index.css">
+
+</head>
+
+<body>
+	<header>
+		<img src="images/index_header.png" alt="">
+	</header>
+	<section>
+		
+
+		<article>
+			<header>
+				<h1>
+					Hola Mundo HTML
+				</h1>				
+				<p>
+					Ejemplo con un hola mundo.
+				</p>
+				<a href="./helloWorldHTML/helloWorld.html"><span class="animation_down_loop fa-arrow-down fa fa-2x"></span></a>
+				
+			</header>
+			<p>
+				Ejemplo para Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate sapiente distinctio ipsum, molestiae doloribus obcaecati accusamus inventore fugit dicta porro quam ipsa reprehenderit asperiores harum aperiam quia totam excepturi sunt.
+			</p>
+		</article>
+
+		<article>
+			<header>
+				<h1>
+					MVC Basico
+				</h1>				
+				<p>
+					Ejemplo simple del MVC (Modelo Vista Controlador).
+				</p>
+				<span class="fa-arrow-down fa fa-2x"></span>
+			</header>
+			<p>
+				Ejemplo para Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate sapiente distinctio ipsum, molestiae doloribus obcaecati accusamus inventore fugit dicta porro quam ipsa reprehenderit asperiores harum aperiam quia totam excepturi sunt.
+			</p>
+		</article>
+
+	</section>
+</body>
+</html>
